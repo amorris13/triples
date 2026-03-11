eebf03d761750d340d5e643dc94b2ff1f9a3e529
Anthony Morris <anthonymorris13@gmail.com>
2026-03-11 13:00:40 +1100

    fix formatting.

--- app/src/main/java/com/antsapps/triples/AchievementManager.java
+++ app/src/main/java/com/antsapps/triples/AchievementManager.java
@@ -6,7 +6,6 @@ import com.antsapps.triples.backend.Application;
 import com.antsapps.triples.backend.ArcadeGame;
 import com.antsapps.triples.backend.ClassicGame;
 import com.antsapps.triples.backend.DailyGame;
-import com.antsapps.triples.backend.Game;
 import com.google.android.gms.games.PlayGames;
 import java.util.ArrayList;
 import java.util.Calendar;
